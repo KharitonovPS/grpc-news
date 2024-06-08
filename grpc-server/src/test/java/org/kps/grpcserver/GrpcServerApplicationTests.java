44bae84a9bbac9c46cbb164f8f70a9af044f9d89
p.kharitonov <p.kharitonov@cn-innov.ru>
2024-06-08 22:02:08 +0300

    init()

---
+++ grpc-server/src/test/java/org/kps/grpcserver/GrpcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kps.grpcserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
