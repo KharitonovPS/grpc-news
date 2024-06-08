15cb2c4693b2bd23be03ec2239920cb1187190ff
p.kharitonov <p.kharitonov@cn-innov.ru>
2024-06-09 00:27:46 +0300

    grpc call

--- grpc-client/src/main/java/org/kps/grpcclient/GrpcClientApplication.java
+++ grpc-client/src/main/java/org/kps/grpcclient/GrpcClientApplication.java
@@ -1,13 +1,37 @@
 package org.kps.grpcclient;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.kps.grpc.TaskService;
+import org.kps.grpc.TasksServiceGrpc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+@Slf4j
 @SpringBootApplication
 public class GrpcClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GrpcClientApplication.class, args);
+        ManagedChannel channel = ManagedChannelBuilder.forTarget("localhost:8080")
+                .usePlaintext()
+                .build();
+
+        TasksServiceGrpc.TasksServiceBlockingStub blockingStub = TasksServiceGrpc.newBlockingStub(channel);
+
+        TaskService.TaskRequest request = TaskService.TaskRequest.newBuilder()
+                .addAllTask(List.of("CREW-01","CARGO-02"))
+                .setDev("Polly")
+                .build();
+
+        TaskService.TaskResponse taskResponse = blockingStub.addTasks(request);
+        log.info("Take response, {}", taskResponse);
+
+        channel.shutdownNow();
+
     }
 
 }
--- grpc-server/src/main/java/org/kps/grpcserver/GrpcServerApplication.java
+++ grpc-server/src/main/java/org/kps/grpcserver/GrpcServerApplication.java
@@ -1,13 +1,29 @@
 package org.kps.grpcserver;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.kps.grpcserver.service.TaskServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
+@Slf4j
 @SpringBootApplication
 public class GrpcServerApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         SpringApplication.run(GrpcServerApplication.class, args);
+        Server server = ServerBuilder.forPort(8080)
+                .addService(new TaskServiceImpl() )
+                .build();
+
+        server.start();
+        log.info("Server started...");
+
+        server.awaitTermination();
     }
 
 }
