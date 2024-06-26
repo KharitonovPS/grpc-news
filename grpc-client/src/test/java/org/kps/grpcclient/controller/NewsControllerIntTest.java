21f545795964ed02ecbb687a4ac71d3788f5a68c
p.kharitonov <p.kharitonov@cn-innov.ru>
2024-06-26 23:40:20 +0300

    add test when app up

--- grpc-client/src/test/java/org/kps/grpcclient/controller/NewsControllerIntTest.java
+++ grpc-client/src/test/java/org/kps/grpcclient/controller/NewsControllerIntTest.java
@@ -1,40 +1,54 @@
 package org.kps.grpcclient.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.kps.grpcclient.service.News;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.graphql.GraphQlTest;
-import org.springframework.graphql.test.tester.GraphQlTester;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.graphql.test.tester.HttpGraphQlTester;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@GraphQlTest(NewsController.class)
+@SpringBootTest
 class NewsControllerIntTest {
 
-    @Autowired
-    GraphQlTester graphQlTester;
+    private HttpGraphQlTester tester;
+
+    @BeforeEach
+    void setUp() {
+        WebTestClient client = WebTestClient.bindToServer()
+                .baseUrl("http://localhost:8080/graphql")
+                .build();
+
+        tester = HttpGraphQlTester.create(client);
+
+    }
 
     @Test
     void newsById() {
+        tester.documentName("newsDetails")
+                .variable("id", "news-1")
+                .execute()
+                .path("newsById")
+                .matchesJson("""
+                                    {
+                                    "id": "news-1",
+                                    "name": "Effective Java",
+                                    "pageCount": 416,
+                                    "rating": "FIVE_STAR",
+                                    "author": {
+                                        "firstName": "Joshua",
+                                        "lastName": "Bloch"
+                                        }
+                                    }
+                        """);
     }
 
     @Test
     void findAllShouldReturnAllNews() {
-        var q = """
-                query {
-                  findAll {
-                    id
-                    name
-                    pageCount
-                  }
-                }
-                """;
-
-
-        graphQlTester.document(q)
+        tester.documentName("allNews")
                 .execute()
                 .path("findAll")
                 .entityList(News.class)
                 .hasSize(3);
-
     }
 
     @Test
