5b7feb0a2e6259463a66050a96a59cdb444d91e6
Павел Харитонов <p.kharitonov@cn-innov.ru>
2024-07-10 00:33:46 +0300

    add util class

--- grpc-client/src/main/java/org/kps/grpcclient/client/NewsClient.java
+++ grpc-client/src/main/java/org/kps/grpcclient/client/NewsClient.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.kps.grpc.NewsServiceGrpc;
 import org.kps.grpc.TaskService;
 import org.kps.grpcclient.mapper.NewsResponseMapper;
+import org.kps.grpcclient.utils.FutureConverter;
 import org.kps.grpcmodel.model.News;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
@@ -35,36 +36,16 @@ public class NewsClient {
     @EventListener(ApplicationReadyEvent.class)
     private void initialize() {
         futureStub = NewsServiceGrpc.newFutureStub(managedChannel);
-//                .withDeadline(Deadline.after(4000L, TimeUnit.MILLISECONDS));
     }
 
     public CompletableFuture<News> findNewsByName(String name) {
         TaskService.NewsSelectByNameRequest request = TaskService.NewsSelectByNameRequest.newBuilder()
                 .setName(name)
                 .build();
-        ListenableFuture<TaskService.NewsResponse> listenableFuture = futureStub
-                .withDeadline(Deadline.after(4000L, TimeUnit.MILLISECONDS))
-                .findNewsByName(request);
-        CompletableFuture<TaskService.NewsResponse> completableFuture = new CompletableFuture<>();
-
-        Futures.addCallback(listenableFuture, new FutureCallback<TaskService.NewsResponse>() {
-            @Override
-            public void onSuccess(@Nullable TaskService.NewsResponse result) {
-                completableFuture.complete(result);
-            }
-
-            @Override
-            public void onFailure(Throwable t) {
-                Status status = Status.fromThrowable(t);
-                if (status.getCode() == Status.Code.DEADLINE_EXCEEDED) {
-                    log.warn("Request timed out");
-                } else {
-                    log.error("Request failed %s".formatted(t.getMessage()), t);
-                    completableFuture.completeExceptionally(t);
-                }
-            }
-        }, new VirtualThreadTaskExecutor(futureStub.getClass() + "-newsClient"));
-
+        var completableFuture = FutureConverter.toCompletableFuture(futureStub.withDeadline(Deadline.after(
+                4000L,
+                TimeUnit.MILLISECONDS
+        )).findNewsByName(request));
         return completableFuture.thenApply(mapper::toNews);
     }
 
---
+++ grpc-client/src/main/java/org/kps/grpcclient/utils/FutureConverter.java
@@ -0,0 +1,41 @@
+package org.kps.grpcclient.utils;
+
+
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import io.grpc.Status;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.task.VirtualThreadTaskExecutor;
+
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+public class FutureConverter {
+
+    private static final VirtualThreadTaskExecutor TASK_EXECUTOR = new VirtualThreadTaskExecutor(
+            FutureConverter.class.getName() + "-pool");
+
+    public static <T> CompletableFuture<T> toCompletableFuture(ListenableFuture<T> listenableFuture) {
+        CompletableFuture<T> completableFuture = new CompletableFuture<>();
+        Futures.addCallback(listenableFuture, new FutureCallback<T>() {
+
+            @Override
+            public void onSuccess(T result) {
+                completableFuture.complete(result);
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Status status = Status.fromThrowable(t);
+                if (status.getCode() == Status.Code.DEADLINE_EXCEEDED) {
+                    log.warn("Request timed out");
+                } else {
+                    log.error("Request failed %s".formatted(t.getMessage()), t);
+                    completableFuture.completeExceptionally(t);
+                }
+            }
+        }, TASK_EXECUTOR);
+        return completableFuture;
+    }
+}
